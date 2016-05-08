ca7001aa6a136c3bb98cba64f53df41f67ba8132
pietrzakacper <kacpietrzak@gmail.com>
2016-05-08 21:47:09 +0200

    Adding ability to change horizontal direction. Adding system of air resistance

--- Source/Player.cpp
+++ Source/Player.cpp
@@ -6,6 +6,8 @@ Player::Player(std::string texturePath, sf::Vector2f velVec, float fuel, float e
 {
 	m_rocketEngineFuel = fuel;
 	m_electricity = elect;
+	m_sprite.setPosition((1280 - m_sprite.getGlobalBounds().width) / 2.f, 0);
+	defaultRotation = m_sprite.getRotation();
 }
 
 void Player::Update(float fps, float wind)
@@ -13,37 +15,56 @@ void Player::Update(float fps, float wind)
 	m_velocityVector.y += gravity;
 	m_velocityVector.x += wind;
 	m_sprite.move(m_velocityVector.x * fps, m_velocityVector.y * fps);
+	if(horizontalInput)m_sprite.rotate(rotateAngleValue *fps);
+	else
+	{
+		if (m_sprite.getRotation() > 180)m_sprite.rotate(5 * fps);
+		else if (m_sprite.getRotation() <180)m_sprite.rotate(-5 * fps);
+	}
+	
+	m_velocityVector.x *= 0.95;
+	
+	
 }
 
 void Player::CheckEvents(sf::Event & event)
 {
-	float engineFuelConsuption = 0.1f;
-	
-	if (event.type == sf::Event::KeyPressed)
-	{
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
-		{
+	float engineFuelConsuption = 0.0f;
 
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::A) && m_velocityVector.x > -300.f)
+		{
+			m_velocityVector.x -= steerForce;
+			rotateAngleValue = -20.f;
+			horizontalInput = true;
 		}
 		
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+		else if (sf::Keyboard::isKeyPressed(sf::Keyboard::D) && m_velocityVector.x < 300.f)
 		{
-
+			m_velocityVector.x += steerForce;
+			rotateAngleValue = 20.f;
+			horizontalInput = true;
+		}
+		else
+		{
+			
+			horizontalInput = false;
 		}
 
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space))
 		{
-			if (m_rocketEngineFuel - engineFuelConsuption < 0)
-			{
-				m_rocketEngineFuel = 0;
-			}
-			else
-			{
-				m_velocityVector.y -= m_TRUST;
-				m_rocketEngineFuel -= engineFuelConsuption;
-			}
-			
-			std::cout << m_rocketEngineFuel << "\n";
+			if (m_velocityVector.y > -120.f)m_velocityVector.y -= m_TRUST;
+
+			//if (m_rocketEngineFuel - engineFuelConsuption < 0)
+			//{
+			//	m_rocketEngineFuel = 0;
+			//}
+			//else
+			//{
+			//	
+			//	m_rocketEngineFuel -= engineFuelConsuption;
+			//}
+			//
+			//std::cout << m_rocketEngineFuel << "\n";
 		}
-	}
+	
 }
--- Source/Player.h
+++ Source/Player.h
@@ -7,8 +7,12 @@ class Player :public GameObject
 {
 	float m_rocketEngineFuel;
 	float m_electricity;
-	const float m_TRUST = 80.f;
+	const float m_TRUST = 20.f;
+	const float steerForce = 20.f;
+	float rotateAngleValue = 0.f;
 	float gravity;
+	float defaultRotation;
+	bool horizontalInput = false;
 
 public:
 	Player(std::string texturePath, sf::Vector2f velVec, float fuel, float elect, float grav);
