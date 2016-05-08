3823054686a0c068f5ebc22aa1c75da0a2e949dd
pietrzakacper <kacpietrzak@gmail.com>
2016-05-08 19:53:59 +0200

    Adding engine activation by spacebar

--- Source/Player.cpp
+++ Source/Player.cpp
@@ -8,11 +8,13 @@ Player::Player(std::string texturePath, sf::Vector2f velVec, float fuel, float e
 
 void Player::Update(float fps, float gravity, float wind)
 {
-	m_sprite.move((m_velocityVector.x += wind) * fps, (m_velocityVector.y += +gravity) * fps);
+	m_sprite.move((m_velocityVector.x += wind) * fps, (m_velocityVector.y += gravity) * fps);
 }
 
 void Player::CheckEvents(sf::Event & event)
 {
+	float engineFuelConsuption = 0.1f;
+	
 	if (event.type == sf::Event::KeyPressed)
 	{
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
@@ -27,7 +29,17 @@ void Player::CheckEvents(sf::Event & event)
 
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space))
 		{
-
+			if (m_rocketEngineFuel - engineFuelConsuption < 0)
+			{
+				m_rocketEngineFuel = 0;
+			}
+			else
+			{
+				m_velocityVector.y -= m_TRUST;
+				m_rocketEngineFuel -= engineFuelConsuption;
+			}
+			
+			std::cout << m_rocketEngineFuel << "\n";
 		}
 	}
 }
--- Source/Player.h
+++ Source/Player.h
@@ -1,11 +1,13 @@
 #pragma once
 
 #include "GameObject.h"
+#include <iostream>
 
 class Player :public GameObject
 {
 	float m_rocketEngineFuel;
 	float m_electricity;
+	const float m_TRUST = 50.f;
 
 public:
 	Player(std::string texturePath, sf::Vector2f velVec, float fuel, float elect);
